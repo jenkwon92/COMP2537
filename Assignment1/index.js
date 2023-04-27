532dbc4c477434735fd4e2a2fb4ab5a277ac6d5c
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-04-27 14:26:58 -0700

    Simple Website playing with routes and URL and query params

--- Assignment1/index.js
+++ Assignment1/index.js
@@ -8,6 +8,30 @@ app.get('/', (req,res) => {
     res.send("<h1>Hello World!</h1>");
 });
 
+app.get('/about', (req,res) => {
+    var color = req.query.color;
+
+    res.send("<h1 style='color:"+color+";'>Yongeun Kwon</h1>");
+});
+
+app.get('/cat/:id', (req,res) => {
+
+    var cat = req.params.id;
+
+    if (cat == 1) {
+        res.send("Yallu1: <img src='/cat1.jpg' style='width:250px;'>");
+    }
+    else if (cat == 2) {
+        res.send("Yallu2: <img src='/cat2.jpg' style='width:250px;'>");
+    }
+    else {
+        res.send("Invalid cat id: "+cat);
+    }
+});
+
+
+app.use(express.static(__dirname + "/public"));
+
 app.listen(port, () => {
 	console.log("Node application listening on port "+port);
 }); 
