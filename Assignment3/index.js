ecf0bcf443687a6e8d50b35afb8e693086427caf
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-05-15 18:36:42 -0700

    Pokemons Grid - Fetch all pokemons names from the API

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -1,6 +1,13 @@
 const setup = async () => {
-  // test out poke api using axios here
-  console.log('test');
-  const res = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
-  console.log(res.data);
-};
+    // test out poke api using axios here
+    $('#pokeCards').empty()
+    let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+    const pokemons = response.data.results;
+    pokemons.forEach(async (pokemon) => {
+      const res = await axios.get(pokemon.url)
+      $('#pokeCards').append(`
+          ${res.data.name.toUpperCase()} 
+          `)
+    })
+  }
+  $(document).ready(setup)
