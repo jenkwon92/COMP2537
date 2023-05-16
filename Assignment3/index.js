95d561ba18c9d3c6ce4df420c16284ade4fdff0a
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-05-15 18:31:46 -0700

    Verify Axios is working

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -1,5 +1,6 @@
 const setup = async () => {
   // test out poke api using axios here
+  console.log('test');
   const res = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
   console.log(res.data);
 };
