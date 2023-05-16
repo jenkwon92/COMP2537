c1e1455ab838e989938fa24a74e9b6767961b984
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-05-15 18:39:13 -0700

    Pokemons Grid - Create a modal to display the detail of the pokemons

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -1,13 +1,23 @@
 const setup = async () => {
-    // test out poke api using axios here
-    $('#pokeCards').empty()
-    let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
-    const pokemons = response.data.results;
-    pokemons.forEach(async (pokemon) => {
-      const res = await axios.get(pokemon.url)
-      $('#pokeCards').append(`
-          ${res.data.name.toUpperCase()} 
-          `)
-    })
-  }
-  $(document).ready(setup)
+  // test out poke api using axios here
+  $("#pokeCards").empty();
+  let response = await axios.get(
+    "https://pokeapi.co/api/v2/pokemon?offset=0&limit=810"
+  );
+  const pokemons = response.data.results;
+  pokemons.forEach(async (pokemon) => {
+    const res = await axios.get(pokemon.url);
+    $("#pokeCards").append(`
+          <div class="pokeCard card"  >
+            <h3>${res.data.name.toUpperCase()}</h3> 
+            <img src="${res.data.sprites.front_default}" alt="${
+      res.data.name
+    }"/>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
+              More
+            </button>
+            </div>  
+            `);
+  });
+};
+$(document).ready(setup);
